0e73f2086f82fc76992974bcd710d967fce8faba
Methmi Nugawela <methmi.nuga@gmail.com>
2020-09-03 18:10:34 +0530

    commit #2

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-
-  final Function selectHandler;  //stores a pointer to a function 
-  final String answer;  
+  final Function selectHandler; //stores a pointer to a function
+  final String answer;
 
   Answer(this.selectHandler, this.answer);
-
 
   @override
   Widget build(BuildContext context) {
